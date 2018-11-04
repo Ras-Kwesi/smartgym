c43082a8e8484fed454757c1d5614ada2e17cefc
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-11-04 14:39:20 +0300

    conflicts in settings resolve

--- smart_gym/settings.py
+++ smart_gym/settings.py
@@ -153,17 +153,9 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-=======
->>>>>>> gift
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 MEDIA_URL = '/media/'
-<<<<<<< HEAD
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-=======
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
->>>>>>> gift
