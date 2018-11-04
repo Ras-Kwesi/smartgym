69c1978289899b7c38cb2791d50d3a94dccca5a4
Owenmur21 <owenmuriithi@gmail.com>
2018-11-04 13:19:35 +0300

    Prepares for merge

--- gym/views.py
+++ gym/views.py
@@ -6,11 +6,11 @@ def index(request):
     """
     Renders the index page
     """
-    if Join.objects.filter(user_id = request.user).exists():
-        gym = Gym.objects.get(pk = request.user.join.gym_id)
-        return render(request, 'hood.html', locals())
+    # if Join.objects.filter(user_id = request.user).exists():
+    #     gym = Gym.objects.get(pk = request.user.join.gym_id)
+    #     return render(request, 'gymnast/home.html', locals())
 
-    else:
-        gyms = Gym.objects.all()
-        return render(request, 'index.html', locals())
+    # else:
+    gyms = Gym.objects.all()
+    return render(request, 'gymnast/index.html', locals())
 
