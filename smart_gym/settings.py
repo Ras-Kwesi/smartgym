531dc381cd8a8867791ade4d01857c6f82c0cb24
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-07 09:48:59 +0300

    fixes login page

--- smart_gym/settings.py
+++ smart_gym/settings.py
@@ -12,21 +12,15 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
+           'NAME': 'smartgym',
+           'USER': 'gift',
+           'PASSWORD': 'gL0711',
+           'HOST': '',
            'PORT': '',
        }
        
    }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
